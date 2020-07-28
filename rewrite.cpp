312ed3365c874fa4d0f9e351f4a1df2319cc7355
Samuel Lord <sllord@icloud.com>
2020-07-28 15:04:15 -0500

    Oops! Forgot to open LUA libs!
    stinky poo.

--- rewrite.cpp
+++ rewrite.cpp
@@ -12,9 +12,15 @@ extern "C"
 
 using namespace std;
 
+void fakeLClosure() {
+    bool LClosure = true;
+}
+
 int main() {
+    fakeLClosure();
     lua_State *L;
     L = luaL_newstate();
+    luaL_openlibs(L);
     luaL_dostring(L, "print('heck yea! it works!'); LClosure = true"); //Since LClosure's proto must return true or false, let's cut right to the chase.
     luaL_dofile(L, "calamari-ios-mods/modded-calamari-ios.lua"); luaL_dostring(L, "LClosure = true"); //Also: do LClosure every time you execute a script
 }
