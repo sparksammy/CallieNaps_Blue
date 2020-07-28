8c4a4b965eb7d79ec7f6bee6158c5e4f38d36098
Samuel Lord <sllord@icloud.com>
2020-07-27 23:02:00 -0500

    Upgrade to get a GUI
    Notice GUI isn't in source, get Calamari GUI and modify it, put modded GUI in code, mod code to remove references to Calamari (but keep the credits ofc), put under proper license.

--- rewrite.cpp
+++ rewrite.cpp
@@ -16,6 +16,7 @@ int main() {
     lua_State *L;
     L = luaL_newstate();
     luaL_dostring(L, "print('heck yea! it works!'); LClosure = true"); //Since LClosure's proto must return true or false, let's cut right to the chase.
+    luaL_dofile(L, "calamari-ios-mods/modded-calamari-ios.lua"); luaL_dostring(L, "LClosure = true"); //Also: do LClosure every time you execute a script
 }
 
 #endif // __LUA_INC_H__
