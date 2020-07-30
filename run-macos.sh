48d52c2ee47b227d351492e0e4efe36d29640808
Samuel Lord <sllord@icloud.com>
2020-07-29 23:00:41 -0500

    Add files via upload

--- run-macos.sh
+++ run-macos.sh
@@ -1,6 +1,8 @@
 echo Injecting Dynamic Library
 echo Thanks to this method here:
 echo https://medium.com/@jon.gabilondo.angulo_7635/how-to-inject-code-into-mach-o-apps-part-i-17ed375f736e
+echo Also:
+echo https://stackoverflow.com/questions/2339679/what-are-the-differences-between-so-and-dylib-on-osx
 echo Modded for ROBLOX by Sparksammy.
 DYLD_INSERT_LIBRARIES=builds/callienaps_blue_main.so /Applications/Roblox.app/Contents/MacOS/RobloxPlayerBeta
 echo Done.
