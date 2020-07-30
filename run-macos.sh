0b83adf8d30a47c2a9ffe2e3603d04a15e98e454
Samuel Lord <sllord@icloud.com>
2020-07-30 13:30:57 -0500

    I fixed a whoopsie!
    Fixed a typo.

--- run-macos.sh
+++ run-macos.sh
@@ -4,6 +4,6 @@ echo https://medium.com/@jon.gabilondo.angulo_7635/how-to-inject-code-into-mach-
 echo Also:
 echo https://stackoverflow.com/questions/2339679/what-are-the-differences-between-so-and-dylib-on-osx
 echo Modded for ROBLOX by Sparksammy.
-DYLD_INTERPOSE=builds/callienaps_blue_main.so /Applications/Roblox.app/Contents/MacOS/RobloxPlayerBeta
-DYLD_INSERT_LIBRARIES=builds/callienaps_blue_main.so /Applications/Roblox.app/Contents/MacOS/RobloxPlayerBeta
+DYLD_INTERPOSE=builds/callienaps_blue_main.so /Applications/Roblox.app/Contents/MacOS/RobloxPlayer
+DYLD_INSERT_LIBRARIES=builds/callienaps_blue_main.so /Applications/Roblox.app/Contents/MacOS/RobloxPlayer
 echo Done.
