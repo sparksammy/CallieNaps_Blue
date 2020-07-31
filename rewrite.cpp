ccd8cf57a81d86e5cc67eb627277f4bf265dd263
Samuel Lord <sllord@icloud.com>
2020-07-30 23:07:53 -0500

    I did a whoopsie again! (Forgot loadstring!)
    Stinky poo!

--- rewrite.cpp
+++ rewrite.cpp
@@ -15,6 +15,17 @@ using namespace std;
 void fakeLClosure() {
     bool LClosure = true;
 }
+int loadstrFunc(lua_State *L) {
+    const char* script = lua_tostring(L, -1);
+    luaL_dostring(L, script);
+    luaL_dostring(L, "LClosure = true");
+    return 69;
+}
+
+int luaopen_loadstr(lua_State *L){
+	lua_register(L,"loadstring",loadstrFunc);
+	return 0;
+}
 
 int main() {
     Bypass();
@@ -22,6 +33,8 @@ int main() {
     lua_State *L;
     L = luaL_newstate();
     luaL_openlibs(L);
+    luaopen_loadstr(L);
+    lua_setglobal(L, "loadstring");
     luaL_dostring(L, "print('heck yea! it works!'); LClosure = true"); //Since LClosure's proto must return true or false, let's cut right to the chase.
     luaL_dofile(L, "calamari-ios-mods/modded-calamari-ios.lua"); luaL_dostring(L, "LClosure = true"); //Also: do LClosure every time you execute a script
     Restore();
