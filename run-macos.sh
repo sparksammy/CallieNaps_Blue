4fe8d573ca29fdec46b65039c3ad0d0066740f6c
Rusty Sparkyford <sllord@icloud.com>
2021-06-12 18:55:25 -0500

    Now is a Fluxus alternative for forked Jellyfish
    This project is now a Fluxus alternative for Jellyfish Mac users, as well as for my education on how the heck ROBLOX Mac Exploits work.

--- run-macos.sh
+++
@@ -1,9 +0,0 @@
-echo Injecting Dynamic Library
-echo Thanks to this method here:
-echo https://medium.com/@jon.gabilondo.angulo_7635/how-to-inject-code-into-mach-o-apps-part-i-17ed375f736e
-echo Also:
-echo https://stackoverflow.com/questions/2339679/what-are-the-differences-between-so-and-dylib-on-osx
-echo Modded for ROBLOX by Sparksammy.
-DYLD_INTERPOSE=builds/callienaps_blue_main.so /Applications/Roblox.app/Contents/MacOS/RobloxPlayer
-DYLD_INSERT_LIBRARIES=builds/callienaps_blue_main.so /Applications/Roblox.app/Contents/MacOS/RobloxPlayer
-echo Done.
