97092bdd89b29e0e2cc9e646f2618ebbff0f0676
Rusty Sparkyford <sllord@icloud.com>
2021-06-12 19:20:04 -0500

    Update compile-mac.sh

--- compile-mac.sh
+++ compile-mac.sh
@@ -1,3 +1,4 @@
 echo Note: this builder is made for macOS 12
-g++ rewrite.cpp -llua5.1 -ILua/ -lm -ldl -o builds/callienaps_blue_main.dylib
+mkdir builds
+g++ rewrite.cpp -llua5.1 -ILua/ -lm -ldl -o builds/callienaps_blue_main.dylib -v
 echo OK
